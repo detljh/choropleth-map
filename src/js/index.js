dd3fb900ff25e490dae152c6e74618697bbf18f5
lisa <lisa.ng8@hotmail.com>
2020-02-03 19:34:07 +1100

    changed scale to go from min to max of data

--- src/js/index.js
+++ src/js/index.js
@@ -8,59 +8,12 @@ function getData(url) {
 
 let width = 960;
 let height= 600;
-let domainNum = 8;
 let svg = d3.select("svg")
             .attr("width", width)
             .attr("height", height);
 
 let path = d3.geoPath();
-
 let data = d3.map();
-
-let x = d3.range(100/domainNum, 100, 100/domainNum);
-let scale = d3.scaleLinear()
-        .domain([0, 100])
-        .range([600, 880]);
-
-const colors = [];
-for (let i = domainNum + 1; i > 0; i--) {
-    let color = `hsl(30, 50%, ${i*(100/(domainNum+1))-5}%)`;
-    colors.push(color);
-}
-
-let colorScale = d3.scaleThreshold()
-    .domain(x)
-    .range(colors);
-
-let legend = svg.append("g")
-                .attr("id", "legend")
-                .attr("transform", "translate(0, 30)");
-
-let scaleWidth = 35;
-
-legend.selectAll("rect")
-    .data(x)
-    .enter()
-    .append("rect")
-    .attr("height", 10)
-    .attr("x", (d, i) => {
-        return (i*scaleWidth) + 600;
-    })
-    .attr("width", scaleWidth)
-    .attr("fill", (d, i) => colors[i]);
-
-
-legend.call(d3.axisBottom(scale)
-                .tickSize(16)
-                .tickFormat((x) => Math.round(x) + "%")
-                .tickValues([0, ...x]))
-                .select(".domain")
-                .remove();
-
-
-let tooltip = d3.select("main")
-                .append("div")
-                .attr("id", "tooltip");
 
 let education = getData("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json");
 let county = getData("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json");
@@ -68,6 +21,58 @@ let county = getData("https://cdn.freecodecamp.org/testable-projects-fcc/data/ch
 Promise.all([education, county]).then((data) => {
     education = data[0];
     county = data[1];
+
+    let bachelors = education.map((d) => d.bachelorsOrHigher);
+    let min = d3.min(bachelors);
+    let max = d3.max(bachelors);
+    let domainNum = 9;
+    let scaleWidth = 34;
+    let step = (max-min)/domainNum;
+    let xShift = 560;
+
+    let x = d3.range(min + step, max+step, step);
+    let scale = d3.scaleLinear()
+            .domain([min, max])
+            .range([xShift, (xShift + domainNum*scaleWidth)]);
+
+    const colors = [];
+    for (let i = domainNum + 1; i > 0; i--) {
+        let color = `hsl(30, 60%, ${i*(100/(domainNum+1))-5}%)`;
+        colors.push(color);
+    }
+
+    let colorScale = d3.scaleThreshold()
+        .domain(x)
+        .range(colors);
+
+    let legend = svg.append("g")
+                    .attr("id", "legend")
+                    .attr("transform", "translate(0, 30)");
+
+    legend.selectAll("rect")
+        .data(x)
+        .enter()
+        .append("rect")
+        .attr("height", 10)
+        .attr("x", (d, i) => {
+            return (i*scaleWidth) + xShift;
+        })
+        .attr("width", scaleWidth)
+        .attr("fill", (d, i) => colors[i]);
+
+
+    legend.call(d3.axisBottom(scale)
+                    .tickSize(16)
+                    .tickFormat((x) => Math.round(x) + "%")
+                    .tickValues([min, ...x]))
+                    .select(".domain")
+                    .remove();
+
+
+    let tooltip = d3.select("main")
+                    .append("div")
+                    .attr("id", "tooltip");
+
     svg.append("g")
         .attr("class", "counties")
         .selectAll("path")
